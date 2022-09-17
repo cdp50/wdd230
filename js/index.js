c39baa9d070828d540e17e3047a7033cffc42207
cdp50 <par17069@byui.edu>
2022-09-17 14:25:52 -0600

    js modified

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
-// sequence
-
-let date = new Date();
-
-console.log(date);
 
 let year = date.getFullYear();
-
-
-// document.querySelector("span").textContent = year;
 document.getElementById("currentYear").textContent = year;
 
 
 
 let currentdate = document.lastModified;
+document.getElementById("lastModified").textContent = currentdate;
 
-console.log (currentdate);
- document.getElementById("lastModified").textContent = currentdate;
-
- let currentYear;
+ 
