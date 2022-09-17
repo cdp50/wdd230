5fe5a85b3fbd05c3fd931aefde7411af565ccaed
cdp50 <par17069@byui.edu>
2022-09-17 14:23:30 -0600

    first assingment

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// sequence
+
+let date = new Date();
+
+console.log(date);
+
+let year = date.getFullYear();
+
+
+// document.querySelector("span").textContent = year;
+document.getElementById("currentYear").textContent = year;
+
+
+
+let currentdate = document.lastModified;
+
+console.log (currentdate);
+ document.getElementById("lastModified").textContent = currentdate;
+
+ let currentYear;
